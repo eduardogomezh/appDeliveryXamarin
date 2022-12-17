6f37a35f94505a9264c78116b590694d0e600512
Eduardo Daniel Gómez Hernández <eduardod.ghz@gmail.com>
2022-12-16 18:12:17 -0600

    agrego baseViewModel

---
+++ appDeliveryXamarin/appDeliveryXamarin/Modelo/MPlatillos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appDeliveryXamarin.Modelo
+{
+    public class MPlatillos
+    {
+        public string descripcion { get; set; }
+        public string precio { get; set; }
+        public string icon { get; set; }
+    }
+}
